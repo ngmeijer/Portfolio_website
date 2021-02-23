505b5f5b24454d4ceae031da1b0af2cb1f036c12
Nils Meijer <meijernils9@gmail.com>
2021-02-23 22:42:55 +0100

    Reworked logos, improved modals

--- Website/Javascript/modalActivation.js
+++ Website/Javascript/modalActivation.js
@@ -1,10 +1,16 @@
 
 let modalArray = document.getElementsByClassName("modal");
+let modalActive = false;
 
 function showModal(pModalIndex) {
-    modalArray[pModalIndex].style.display = "block";
+    if (modalActive == false) {
+        modalArray[pModalIndex].style.display = "block";
+        modalArray[pModalIndex].style
+        modalActive = true;
+    }
 }
 
 function closeModal(pModalIndex) {
     modalArray[pModalIndex].style.display = "none";
+    modalActive = false;
 }
